be1b51daef2dfe92fdf8db0f097fe5fb5f221fc8
Donatas991 <m.donrim@gmail.com>
2025-12-01 09:42:33 +0200

    Add files via upload

---
+++ Kontrolinis.py
@@ -0,0 +1,101 @@
+ ## Kontrolinis darbas iš Python programavimo
+ ## Praktinė dalis 25-40 min
+
+ # ----------------------------------------
+#  #
+#  #  STRUKTŪRA:
+#  #  1. Kodo supratimo klausimai #  2. Mažos programavimo užduotys
+#  #  3. Didžioji programavimo užduotis
+#  #
+#  #  Atsakymus rašykite tiesiog šiame faile.
+#  #
+#  #  Priminimas: pažymėjus kelias eilutes, jas užkoduoti galima su ctrl + ?
+#  # ----------------------------------------
+
+#  # ========================================
+#  # 1 DALIS – KODO SUPRATIMO KLAUSIMAI
+#  # ========================================
+# # KLAUSIMAS 1. Kodėl antram kintamajam naudojama int() funkcija?
+#  vardas = input("Koks tavo vardas? ")
+#  skaičius = int(input("Įvesk skaičių nuo 1 iki 100: "))
+
+#  print("Labas", vardas, "tavo skaičius", skaičius * 5)
+#  # ATSAKYMAS: kad neuzkaitytu vieno atsakymo kaip abieju
+
+#  # KLAUSIMAS 2. Kam iš esmės reikalinga eilutė i = i + 1 ?
+#  i = 0
+#  while i < 5: 
+#      print(i)
+#      i = i + 1 
+
+#  # ATSAKYMAS:  kad i reiksme keistusi
+
+#  # KLAUSIMAS 3. Ką darys žemiau esančio kodo dalis?
+
+#  password = ''
+#  while password != "Coding_is_fun!":
+#      password = input()
+
+#  # ATSAKYMAS: reikes kad ivestum pasworda ir ji patikrins jei neteisngai parasys false
+
+#  # KLAUSIMAS 4. 
+#  # Perskaityk šį kodą ir parašyk:
+#  # - ką programa atspausdins,
+#  # - paaiškink savo žodžiais, kodėl taip gaunasi.
+
+#  x = 4
+#  numbers = [10, 20, 30, 40, 50]
+#  result = numbers[x % 3]
+
+# Ką atspausdins programa? 2
+# Paaiškinimas, kodėl taip gaunasi (2–3 sakiniai): % zenklas reiskia dalyba su liekanom pvz: 5%2= parasys 1 nes 5/2 =2 ir liekana 1. Tai 50 / 3 = 16 ir liekana 2 tai atspauzdins2
+
+
+# ========================================
+# 2 DALIS – MAŽOS PROGRAMAVIMO UŽDUOTYS
+# ========================================
+
+# UŽDUOTIS 1.
+# Užpildyk trūkstamą kodo dalį: funkcija turi suskaičiuoti, kiek kartų sąraše pasirodo elementas target.
+# def count_occurrences(lst, target):
+#     count = 0
+#     for item in lst:
+#         # TODO: Pridėti trūkstamas eilutes
+#         pass
+#     return count
+#     print(count)
+
+
+# UŽDUOTIS 2.
+# Parašyk vieną eilutę, kuri pridėtų skaičių 99 į sąrašo pabaigą.
+numbers = [1, 2, 3, 4]
+def insert(numbers(99))
+# TODO: Viena eilutė:
+
+UŽDUOTIS 3.
+ # Pakeisk kodą taip, kad atspausdintų TAU tinkantį sakinį (turi išsirinkti 1 arba 2 sakinio pabaigos žodžius po dvitašakio).
+ Negali keisti kintamojo "tekstas"
+
+tekstas = "Man programuoti su Python sekasi: labai gerai blogai neblogai"
+ print(tekstas[:] + tekstas[:])
+
+#  # ========================================
+#  # 3 DALIS – DIDŽIOJI UŽDUOTIS
+#  # ========================================
+
+#  """
+#  Sukurk programą.
+#  Jei kažkurio žingsnio padaryti negali, pakeisk jį paprastesniu, kad galėtum dirbti toliau.
+
+#  Programa turi:
+#  1. Paprašyti vartotojo įvesti N skaičių (kiek skaičių įves).
+#  2. Sukurti tuščią sąrašą.
+#  3. For ciklu surinkti visus įvestus skaičius į sąrašą. 
+#  4. Suskaičiuoti, kiek sąraše yra lyginių skaičių.
+#  5. Surikiuoti sąrašo elementus didėjimo tvarka su bubble sort.
+#  6. Paslinkti visus saraso elementus į kairę (kokiu nori būdu).
+#  7. Aiškiai atspausdinti rezultatus.
+
+#  """
+
+#  # TODO: Tavo sprendimas:
